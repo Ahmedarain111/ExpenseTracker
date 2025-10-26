981b89813cbd56810c96b57d65cc96d8c013e4f2
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-10-26 11:07:41 +0500

    Change URL name for better understanding

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard_view, name="dashboard"),
     path('transactions/', views.transactions_view, name="transactions"),
     path('accounts/', views.accounts_view, name="accounts"),
-    path('add/', views.add_transaction, name="add_transaction"),
+    path('add_transaction/', views.add_transaction, name="add_transaction"),
     path('add_wallet/', views.add_wallet, name="add_wallet")
 ] 
