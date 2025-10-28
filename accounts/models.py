b3c7df40d3e7890d05382f60fe0a9103662a8c1d
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-10-28 08:35:02 +0500

    Add more fields to Profile

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
     currency = models.CharField(max_length=10, default="PKR")
     
     def __str__(self):
